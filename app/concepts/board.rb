90f61e244f5db14c5492c6a5988d03ae7b90de02
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-10 16:29:02 +1300

    Modify the board class to return a query-able cartegian coordinates based checkers board

--- app/concepts/board.rb
+++ app/concepts/board.rb
@@ -1,17 +1,62 @@
 class Board
   def initialize(squares)
-    @squares = squares.flatten
+    @squares = squares
   end
 
-  Square = Struct.new(:position, :player_colour)
+  Square = Struct.new(:position, :coordinates, :occupant)
 
-  def layout
-    @squares.in_groups_of(2).map do |position, player_colour|
-      Square.new(position, player_colour)
+  def cartesian_layout
+    y_coord = 8
+
+    rows_with_duds.map do |row|
+      x_coord = 1
+      new_layout = row.map do |square|
+        square.coordinates = [x_coord, y_coord]
+        x_coord += 1
+        square
+      end
+      y_coord -= 1
+      new_layout
+    end.flatten
+  end
+
+  def square_by_coordinates(x_coord, y_coord)
+    cartesian_layout.find { |square| square.coordinates == [x_coord, y_coord] }
+  end
+
+  def square_by_position(pos)
+    cartesian_layout.find { |square| square.position == pos }
+  end
+
+  private
+
+  def rows_with_duds
+    hashed_layout.in_groups_of(8).map do |square_1, square_2, square_3, square_4, square_5, square_6, square_7, square_8|
+      [square_1, square_2, square_3, square_4, square_5, square_6, square_7, square_8]
     end
   end
 
-  def active_pieces_count(player)
-    layout.count {|square| square.player_colour == player.colour}
+  def hashed_layout
+    multidimensional_layout_with_duds.flatten.in_groups_of(2).map do |position, occupant|
+      Square.new(position, "unknown", occupant)
+    end
+  end
+
+  def multidimensional_layout_with_duds
+    row_count = 0
+
+    @squares.in_groups_of(4).map do |square_1, square_2, square_3, square_4|
+      row = [square_1, square_2, square_3, square_4]
+      dud_array = [nil, nil, nil, nil]
+      duds = dud_array.map { |pos| [pos, "dud"]  }
+
+      if row_count == 0 || row_count % 2 == 0
+        row_count += 1
+        duds.zip(row)
+      else
+        row_count +=1
+        row.zip(duds)
+      end
+    end
   end
 end
--- spec/concepts/board_spec.rb
+++ spec/concepts/board_spec.rb
@@ -1,29 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Board do
-  let(:squares) { [[1, "red"], [2, "red"], [32, "white"]] }
+  let(:squares) { [[1, "red"], [2, "red"], [3, "red"], [4, "red"]] }
   subject { Board.new(squares) }
   let(:game) { Game.create! }
   let(:player_one) { game.players.create!(colour: :red) }
   let(:player_two) { game.players.create!(colour: :white) }
 
-  describe "#layout" do
-    it "returns an array of square structs" do
-      square_one = Board::Square.new(squares[0][0], squares[0][1])
-      square_two = Board::Square.new(squares[1][0], squares[1][1])
-      square_three = Board::Square.new(squares[2][0], squares[2][1])
+  describe "#cartesian_layout" do
+    it "returns an array of square structs with correct positions, coordinates and occupants" do
+      dud_one = Board::Square.new(nil, [1,8], "dud")
+      square_one = Board::Square.new(squares[0][0], [2,8], squares[0][1])
+      dud_two = Board::Square.new(nil, [3,8], "dud")
+      square_two = Board::Square.new(squares[1][0], [4,8], squares[1][1])
+      dud_three = Board::Square.new(nil, [5,8], "dud")
+      square_three = Board::Square.new(squares[2][0], [6,8], squares[2][1])
+      dud_four = Board::Square.new(nil, [7,8], "dud")
+      square_four = Board::Square.new(squares[3][0], [8,8], squares[3][1])
 
-      expect(subject.layout).to eql [square_one, square_two, square_three]
+      expect(subject.cartesian_layout).to eql [dud_one, square_one, dud_two, square_two, dud_three, square_three, dud_four, square_four]
     end
   end
 
-  describe "#active_pieces_count" do
-    it "returns the count of red pieces on the board" do
-      expect(subject.active_pieces_count(player_one)).to eql 2
-    end
+  describe "#square_by_coordinates" do
+    it "returns the correct square, given a set of cartesian coordinates" do
+      square_four = Board::Square.new(squares[3][0], [8,8],  squares[3][1])
 
-    it "returns the count of white pieces on the board" do
-      expect(subject.active_pieces_count(player_two)).to eql 1
+      expect(subject.square_by_coordinates(8,8)).to eq square_four
+    end
+  end
+
+  describe "#square_by_position" do
+    it "returns the correct square, given a checker board position" do
+      square_four = Board::Square.new(squares[3][0], [8,8],  squares[3][1])
+
+      expect(subject.square_by_position(4)).to eq square_four
     end
   end
 end

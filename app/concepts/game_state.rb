f8da211e4bcd074f71f6b880973ec54c67eeb74b
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-12 15:57:11 +1300

    Add Game State concept and service

---
+++ app/concepts/game_state.rb
@@ -0,0 +1,54 @@
+class GameState
+  attr_reader :board, :game
+  attr_accessor :current_player
+
+  def initialize(game, board, current_player)
+    @game = game
+    @player_one = game.players.first
+    @player_two = game.players.second
+    @board = board
+    @current_player = current_player
+  end
+
+  def over?
+    winner_found? || draw?
+  end
+
+  def winner
+    if winner_found?
+      @game.players.find { |player| player != loser }
+    end
+  end
+
+  def loser
+    if winner_found?
+      player_lost?(@player_one) ? @player_one : @player_two
+    end
+  end
+
+  def draw?
+    !legal_moves_left_for_player?(@player_one) && !legal_moves_left_for_player?(@player_two)
+  end
+
+  private
+
+  def winner_found?
+    player_lost?(@player_one) || player_lost?(@player_two)
+  end
+
+  def player_lost?(player)
+    !active_pieces_left_for_player?(player) || !legal_moves_left_for_player?(player)
+  end
+
+  def active_pieces_left_for_player?(player)
+    active_pieces_count(player) > 0
+  end
+
+  def active_pieces_count(player)
+    @board.cartesian_layout.count {|square| square.occupant == player.colour}
+  end
+
+  def legal_moves_left_for_player?(player)
+    true
+  end
+end
---
+++ app/services/build_game_state.rb
@@ -0,0 +1,63 @@
+class BuildGameState
+  def initialize(game)
+    @game = game
+    @errors = []
+  end
+
+  def call
+    player_turns_and_colours.inject(base_state) do |current_game_state, player_steps|
+      service = TakeTurn.new(game_state: current_game_state, player_colour: player_steps[0], steps: player_steps[1])
+      service.call
+
+      puts "BUILD GAME STATE LOOP"
+      puts "CURRENT CHUNK: #{player_steps}"
+      puts "++++++++++++++++++++++++++++++"
+
+      @errors + service.errors
+      service.game_state
+    end
+  end
+
+  private
+
+  def player_turns_and_colours
+    all_steps = @game.steps.order(:id)
+    chunks = []
+
+    all_steps.chunk do |step|
+      step.player == red_player
+    end.each do |red_player, steps|
+      chunks.push [red_player, steps]
+    end
+
+    turn_steps(chunks)
+  end
+
+  def turn_steps(chunks)
+    chunks.map do |chunk|
+      if chunk[0] == true
+        chunk[0] = "red"
+        chunk
+      elsif chunk[0] == false
+        chunk[0] = "white"
+        chunk
+      end
+    end
+  end
+
+  def base_state
+    GameState.new(@game, starting_board, red_player)
+  end
+
+  def starting_board
+    red_occupied = (1..12).to_a.map { |number| [number, "red"]  }
+    empty = (13..20).to_a.map { |number| [number, "empty"]  }
+    white_occupied = (21..32).to_a.map { |number| [number, "white"]  }
+
+    Board.new(red_occupied + empty + white_occupied)
+  end
+
+  def red_player
+    @game.players.find { |player| player.colour == "red" }
+  end
+end

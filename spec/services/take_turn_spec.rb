6bde280ad5d71a16bff227398430041150d8a98b
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-22 23:33:20 +1300

    Cover the case of only 1 jump step in take turn spec

--- spec/services/take_turn_spec.rb
+++ spec/services/take_turn_spec.rb
@@ -22,6 +22,24 @@ RSpec.describe TakeTurn do
         end
       end
 
+      context "when only one jump step is required to complete a turn" do
+        before do
+          player_one.steps.create!(kind: :simple, from: 12, to: 16)
+          player_one.steps.create!(kind: :simple, from: 16, to: 19)
+        end
+
+        let(:steps) { [player_two.steps.create!(kind: :jump, from: 24, to: 15)] }
+
+        it "applies the step and returns the new game state without errors" do
+          service = TakeTurn.new(game_state: base_game_state, player_colour: player_two.colour, steps: steps)
+          service.call
+
+          expect(service.errors).to be_empty
+          expect(service.game_state.current_player).to eq player_one
+          expect(service.game_state.board.layout).to eql [Board::Square.new(1, "red", [5, 6]), Board::Square.new(2, "red", [6, 7]), Board::Square.new(3, "red", [7, 8]), Board::Square.new(4, "red", [8]), Board::Square.new(5, "red", [1, 9]), Board::Square.new(6, "red", [1, 2, 9, 10]), Board::Square.new(7, "red", [2, 3, 10, 11]), Board::Square.new(8, "red", [3, 4, 11, 12]), Board::Square.new(9, "red", [5, 6, 13, 14]), Board::Square.new(10, "red", [6, 7, 14, 15]), Board::Square.new(11, "red", [7, 8, 15, 16]), Board::Square.new(12, "empty", [8, 16]), Board::Square.new(13, "empty", [9, 17]), Board::Square.new(14, "empty", [9, 10, 17, 18]), Board::Square.new(15, "white", [10, 11, 18, 19]), Board::Square.new(16, "empty", [11, 12, 19, 20]), Board::Square.new(17, "empty", [13, 14, 21, 22]), Board::Square.new(18, "empty", [14, 15, 22, 23]), Board::Square.new(19, "empty", [15, 16, 23, 24]), Board::Square.new(20, "empty", [16, 24]), Board::Square.new(21, "white", [17, 25]), Board::Square.new(22, "white", [17, 18, 25, 26]), Board::Square.new(23, "white", [18, 19, 26, 27]), Board::Square.new(24, "empty", [19, 20, 27, 28]), Board::Square.new(25, "white", [21, 22, 29, 30]), Board::Square.new(26, "white", [22, 23, 30, 31]), Board::Square.new(27, "white", [23, 24, 31, 32]), Board::Square.new(28, "white", [24, 32]), Board::Square.new(29, "white", [25]), Board::Square.new(30, "white", [25, 26]), Board::Square.new(31, "white", [26, 27]), Board::Square.new(32, "white", [27, 28])]
+        end
+      end
+
       context "when multiple jump steps are required to complete a turn" do
         before do
           player_one.steps.create!(kind: :simple, from: 12, to: 16)

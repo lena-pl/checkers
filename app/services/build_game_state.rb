ce485105bea05b1d2f1284b1f947ce0b188bbcc3
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-24 14:07:32 +1300

    Refactor game state and remove validator-related checks from spec

--- app/services/build_game_state.rb
+++ app/services/build_game_state.rb
@@ -1,5 +1,5 @@
 class BuildGameState
-  attr_reader :errors
+  attr_reader :game, :errors
 
   def initialize(game)
     @game = game
@@ -7,11 +7,12 @@ class BuildGameState
   end
 
   def call
-    player_turns_and_colours.inject(base_state) do |current_game_state, player_steps|
-      service = TakeTurn.new(game_state: current_game_state, player: player_steps[0], steps: player_steps[1])
+    player_turns_and_colours.inject(base_state) do |current_game_state, (player, steps)|
+      service = TakeTurn.new(game_state: current_game_state, player: player, steps: steps)
       service.call
 
       @errors += service.errors
+
       service.game_state
     end
   end
@@ -19,32 +20,13 @@ class BuildGameState
   private
 
   def player_turns_and_colours
-    all_steps = @game.steps.order(:id)
-    steps_chunked_by_player = []
-
-    all_steps.chunk do |step|
-      step.player == red_player
-    end.each do |red_player, steps|
-      steps_chunked_by_player.push [red_player, steps]
-    end
-
-    turn_steps(steps_chunked_by_player)
-  end
-
-  def turn_steps(steps_chunked_by_player)
-    steps_chunked_by_player.map do |chunk|
-      if chunk[0] == true
-        chunk[0] = @game.players.find { |player| player.colour == "red" }
-        chunk
-      elsif chunk[0] == false
-        chunk[0] = @game.players.find { |player| player.colour == "white" }
-        chunk
-      end
+    game.steps.ordered.chunk(&:player).map do |player, steps|
+      [player, steps]
     end
   end
 
   def base_state
-    GameState.new(@game, starting_board, red_player)
+    GameState.new(game, starting_board, red_player)
   end
 
   def starting_board
@@ -56,6 +38,6 @@ class BuildGameState
   end
 
   def red_player
-    @game.players.find { |player| player.colour == "red" }
+    game.players.find { |player| player.colour == "red" }
   end
 end
--- spec/services/build_game_state_spec.rb
+++ spec/services/build_game_state_spec.rb
@@ -69,30 +69,12 @@ RSpec.describe BuildGameState do
 
     describe "when a player tries to take an illegal step" do
       it "returns not your turn error if wrong player" do
-        step = player_two.steps.create!(kind: :simple, from: 23, to: 18)
+        player_two.steps.create!(kind: :simple, from: 23, to: 18)
 
         service = BuildGameState.new(game)
         service.call
 
         expect(service.errors).to eq ["It's not your turn right now!"]
-      end
-
-      it "returns invalid jump path error when trying to make illegal jump move" do
-        step = player_one.steps.create!(kind: :jump, from: 12, to: 20)
-
-        service = BuildGameState.new(game)
-        service.call
-
-        expect(service.errors).to eq ["That is not a valid jump path!"]
-      end
-
-      it "returns simple move errors on invalid move" do
-        step = player_one.steps.create!(kind: :simple, from: 12, to: 11)
-
-        service = BuildGameState.new(game)
-        service.call
-
-        expect(service.errors).to eq ["You can only move diagonally!", "You can only move forward!", "That square is occupied!"]
       end
     end
   end

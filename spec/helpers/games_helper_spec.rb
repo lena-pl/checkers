96082da51a59fbd88da0f3cef77617c9fc06ea5c
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-24 14:23:24 +1300

    Add games helper spec

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe GamesHelper, type: :helper do
+  describe "#display_draught_man_entity" do
+    it "returns the correct draught man entity for given colour" do
+      expect(helper.display_draught_man_entity("white")).to eq "&#9920;"
+      expect(helper.display_draught_man_entity("red")).to eq "&#9922;"
+    end
+  end
 end

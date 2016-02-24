bd457cfc8d4cc1253c9f96c812ed8634121bf691
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-24 14:09:14 +1300

    Refactor take turn instance variables for attr readers, add spec for no steps, make spec compatible with construct board service

--- app/services/take_turn.rb
+++ app/services/take_turn.rb
@@ -1,5 +1,5 @@
 class TakeTurn
-  attr_reader :errors, :game_state
+  attr_reader :game_state, :steps, :board, :player, :errors
 
   def initialize(game_state:, player:, steps:)
     @game_state = game_state
@@ -10,45 +10,44 @@ class TakeTurn
   end
 
   def call
-    if player_allowed_to_move?
-      @board = apply_steps
+    if player_allowed_to_move? && steps.any?
+      board = apply_steps
 
-      @errors.push "You must make all available jumps to complete your turn" if @steps.last.kind == "jump" && more_pieces_can_be_captured?
+      errors.push "You must make all available jumps to complete your turn" if steps.last.jump? && more_pieces_can_be_captured?
 
-      find_next_player if @errors.empty?
+      find_next_player if errors.empty?
     else
-      @errors.push "It's not your turn right now!"
+      errors.push "It's not your turn right now!"
     end
 
-    @errors.uniq
-    @game_state.board = @board
+    errors.uniq!
+    game_state.board = board
 
-    @game_state
+    game_state
   end
 
   private
 
   def player_allowed_to_move?
-    @game_state.current_player == @player
+    game_state.current_player == player
   end
 
   def apply_steps
-    @steps.inject(@board) do |current_board, step|
+    steps.inject(board) do |current_board, step|
       service = ApplyStep.new(current_board, step)
       service.call
 
-      @errors += service.errors
       service.board
     end
   end
 
   def find_next_player
-    @game_state.current_player = @game_state.game.players.find { |player| player != @player }
+    game_state.current_player = game_state.game.players.find { |player| player != @player }
   end
 
   def more_pieces_can_be_captured?
-    current_destination = @steps.last.to
+    current_destination = steps.last.to
 
-    AvailableTurns.new(player: @player, board: @board, piece_position: current_destination).more_jump_moves_available?
+    AvailableTurns.new(player: player, board: board, piece_position: current_destination).more_jump_moves_available?
   end
 end

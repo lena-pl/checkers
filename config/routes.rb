e5d6de50ccb4f3d2cb75b76a6d8347aab986bffa
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-24 14:08:00 +1300

    Remove unnecessary route for steps

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :games, only: [:new, :create, :show] do
     resource :player do
-      resources :steps, only: [:new, :create]
+      resources :steps, only: [:create]
     end
   end
 

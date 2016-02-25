2862af0c61498c2f45f90cfb5a29fd1df6193187
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-25 15:24:31 +1300

    Add specs for displaying kings

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe GamesHelper, type: :helper do
       expect(helper.display_draught_man_entity(ConstructBoard::Piece.new("man", "white"))).to eq "&#9920;"
       expect(helper.display_draught_man_entity(ConstructBoard::Piece.new("man", "red"))).to eq "&#9922;"
     end
+
+    it "returns the correct draught king entity for given colour" do
+      expect(helper.display_draught_man_entity(ConstructBoard::Piece.new("king", "white"))).to eq "&#9921;"
+      expect(helper.display_draught_man_entity(ConstructBoard::Piece.new("king", "red"))).to eq "&#9923;"
+    end
   end
 end

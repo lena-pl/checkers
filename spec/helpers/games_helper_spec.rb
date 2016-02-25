337a093ab9d10c487137cbf2705be4952804e890
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-25 14:48:01 +1300

    Update games helper spec to work with piece instead of colour string

--- spec/helpers/games_helper_spec.rb
+++ spec/helpers/games_helper_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe GamesHelper, type: :helper do
   describe "#display_draught_man_entity" do
     it "returns the correct draught man entity for given colour" do
-      expect(helper.display_draught_man_entity("white")).to eq "&#9920;"
-      expect(helper.display_draught_man_entity("red")).to eq "&#9922;"
+      expect(helper.display_draught_man_entity(ConstructBoard::Piece.new("man", "white"))).to eq "&#9920;"
+      expect(helper.display_draught_man_entity(ConstructBoard::Piece.new("man", "red"))).to eq "&#9922;"
     end
   end
 end

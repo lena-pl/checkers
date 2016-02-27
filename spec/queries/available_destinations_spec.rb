1ed10e6bfde482542574bdc42392889d145cedc2
Lena Plaksina <lena.plaksina@gmail.com>
2016-02-27 20:47:17 +1300

    Combine the step validator and available turns service into single query

--- spec/queries/available_destinations_spec.rb
+++ spec/queries/available_destinations_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ValidateStep do
+RSpec.describe AvailableDestinations do
   let(:squares) { [[1, "red"], [2, "red"], [3, "red"], [4, "red"], [5, "red"], [6, "red"], [7, "red"], [8, "red"], [9, "red"], [10, "red"], [11, "red"], [12, "red"], [13, "empty"], [14, "empty"], [15, "empty"], [16, "empty"], [17, "empty"], [18, "empty"], [19, "empty"], [20, "empty"], [21, "white"], [22, "white"], [23, "white"], [24, "white"], [25, "white"], [26, "white"], [27, "white"], [28, "white"], [29, "white"], [30, "white"], [31, "white"], [32, "white"]] }
   let(:board) { Board.new(squares) }
 
@@ -13,10 +13,10 @@ RSpec.describe ValidateStep do
       context "when the step is a legal simple step" do
         let(:step) { player_one.steps.create!(kind: :simple, from: 12, to: 16) }
 
-        it "returns true" do
-          service = ValidateStep.new(board, player_one, step)
+        it "contains the destination as available" do
+          service = AvailableDestinations.new(board, player_one, step.from)
 
-          expect(service.call).to eq true
+          expect(service.call).to include step.to
           expect(service.errors).to be_empty
         end
       end
@@ -24,11 +24,10 @@ RSpec.describe ValidateStep do
       context "when the step is an illegal simple step" do
         let(:step) { player_one.steps.create!(kind: :simple, from: 12, to: 8) }
 
-        it "returns correct errors and false" do
-          service = ValidateStep.new(board, player_one, step)
+        it "does not contain the destination" do
+          service = AvailableDestinations.new(board, player_one, step.from)
 
-          expect(service.call).to eq false
-          expect(service.errors).to eq ["You can only move forward!", "That square is occupied!"]
+          expect(service.call).not_to include step.to
         end
       end
 
@@ -37,10 +36,10 @@ RSpec.describe ValidateStep do
 
         let(:step) { player_two.steps.create!(kind: :jump, from: 24, to: 15) }
 
-        it "returns true" do
-          service = ValidateStep.new(board, player_two, step)
+        it "contains the destination as available" do
+          service = AvailableDestinations.new(board, player_two, step.from)
 
-          expect(service.call).to eq true
+          expect(service.call).to include 15
           expect(service.errors).to be_empty
         end
       end
@@ -48,29 +47,48 @@ RSpec.describe ValidateStep do
       context "when the step is an illegal jump step" do
         let(:step) { player_one.steps.create!(kind: :jump, from: 12, to: 19) }
 
-        it "returns errors and false" do
-          service = ValidateStep.new(board, player_one, step)
+        it "does not contain the destination" do
+          service = AvailableDestinations.new(board, player_one, step.from)
 
-          expect(service.call).to eq false
-          expect(service.errors).to eq ["That is not a valid jump path!"]
+          expect(service.call).not_to include step.to
         end
       end
     end
 
     context "when the piece is a king" do
-      let(:squares) { [[1, "red"], [2, "red"], [3, "red"], [4, "red"], [5, "red"], [6, "red"], [7, "red"], [8, "red"], [9, "red"], [10, "red"], [11, "red"], [12, "empty"], [13, "empty"], [14, "empty"], [15, "empty"], [16, "empty"], [17, "empty"], [18, "empty"], [19, "empty"], [20, "empty"], [21, "white"], [22, "white"], [23, "white"], [24, "white"], [25, "white"], [26, "white"], [27, "white"], [28, "red"], [29, "white"], [30, "white"], [31, "white"], [32, "empty"]] }
+      before do
+        player_one.steps.create!(kind: :simple, from: 12, to: 16)
+        player_two.steps.create!(kind: :simple, from: 23, to: 18)
 
-      let(:crowning_step) { player_one.steps.create!(kind: :simple, from: 28, to: 32) }
+        player_one.steps.create!(kind: :simple, from: 16, to: 19)
+        player_two.steps.create!(kind: :simple, from: 18, to: 14)
+
+        player_one.steps.create!(kind: :simple, from: 11, to: 15)
+        player_two.steps.create!(kind: :simple, from: 27, to: 23)
+
+        player_one.steps.create!(kind: :simple, from: 9, to: 13)
+        player_two.steps.create!(kind: :simple, from: 32, to: 27)
+
+        player_one.steps.create!(kind: :jump, from: 10, to: 17)
+        player_two.steps.create!(kind: :simple, from: 23, to: 18)
+
+        player_one.steps.create!(kind: :simple, from: 8, to: 12)
+        player_two.steps.create!(kind: :simple, from: 18, to: 14)
+
+        player_one.steps.create!(kind: :simple, from: 19, to: 23)
+        player_two.steps.create!(kind: :simple, from: 14, to: 9)
+
+        player_one.steps.create!(kind: :jump, from: 23, to: 32)
+        player_two.steps.create!(kind: :simple, from: 24, to: 20)
+      end
 
       context "when a crowned piece tries to move in that colour's wrong direction" do
-        let(:step) { player_one.steps.create!(kind: :simple, from: 32, to: 28) }
+        let(:board) { BuildGameState.new(game).call.board }
 
-        it "returns true" do
-          ApplyStep.new(board, crowning_step).call
+        it "contains the destination as available" do
+          service = AvailableDestinations.new(board, player_one, 32)
 
-          service = ValidateStep.new(board, player_one, step)
-
-          expect(service.call).to eq true
+          expect(service.call).to include 27
           expect(service.errors).to be_empty
         end
       end
@@ -79,10 +97,10 @@ RSpec.describe ValidateStep do
     context "when the player tries to move a piece that does not belong to them" do
       let(:step) { player_one.steps.create!(kind: :jump, from: 24, to: 19) }
 
-      it 'returns the correct errors' do
-        service = ValidateStep.new(board, player_one, step)
-        service.call
+      it 'returns no destinations and the correct errors' do
+        service = AvailableDestinations.new(board, player_one, step.from)
 
+        expect(service.call).to be_empty
         expect(service.errors).to eq ["That square doesn't hold one of your pieces!"]
       end
     end
